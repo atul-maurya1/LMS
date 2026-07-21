88e3e63feaf3226250ec543457f7913d82edef39
atul1code <atul.urx@gmail.com>
2026-07-21 19:27:09 +0530

    course controled, delete and update

--- server/controller/course.controller.js
+++ server/controller/course.controller.js
@@ -4,8 +4,8 @@ import fs from 'fs'
 import cloudinary from 'cloudinary'
 import asyncHandler from '../utils/asyncHandler.js'
 
-export const createCourse = asyncHandler(async (req, res) => {
 	// only INSTRACTOR
+export const createCourse = asyncHandler(async (req, res) => {
 	const instructor = req.user.id;
 	const {
 		title,
@@ -67,220 +67,65 @@ export const createCourse = asyncHandler(async (req, res) => {
 });
 
 
+// only INSTRACTOR
+export const updateCourse = asyncHandler(async(req, res) => {
+    const courseId = req.params
+   	const {
+		title,
+		subtitle,
+		description,
+		price,
+	} = req.body;
 
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-export const getLecturesByCourseId = async (req, res, next) => {
-   const {id} = req.params // getting id from the url
-   console.log("course id is: ", id)
-  try{
-     const course  = await Course.findById(id) 
-     if(!course){
-        return next (new AppError('course not found', 400))
-     }  
-
-     res.status(200).json({
-        success: true,
-        message: 'course found successfully',
-        course
-     })
-  }catch(e){
-    return next(new AppError(e.message, 400))
-  }
-
-}
-
-
-
-
-
-export const updateCourse = async(req, res, next) => {
-   //   console.log(req.body)
-    const {id} = req.params
-    console.log("update course: ", id)
-
-    const{title, decription, category} = req.body
-      
-        const course = await Course.findById(id)
-        if(!course){
-            return next(new AppError('course not found', 400))
-        } 
-          
-            course.title = title
-            course.decription = decription
-            course.category = category
-
-           if(req.file){
-             if(course.thumbnail?.public_id){
-               await cloudinary.v2.uploader.destroy(course.thumbnail.public_id)
-            }
-          try{
-              const result = await cloudinary.v2.uploader.upload(req.file.path, {
-                folder: 'lms'
-              })
-
-              console.log("result is: ", result)
-
-              course.thumbnail = {
-                public_id: result.public_id,
-                secure_url: result.secure_url
-            }
-
-            fs.rmSync(`uploads/${req.file.filename}`);
-
-            }catch(e){
-               return next(new AppError(e.message, 400))
-            }
-        }
-        
-          await course.save()
-          res.status(200).json({
-                seccess: true,
-                message: 'course updated successfully',
-                course
-            })
-}
-
-
-
-
-export const removeCourse = async (req, res, next) => {
-
-  console.log("delete course")
-  const{id} = req.params
-
-  try{
-    const course = await Course.findByIdAndDelete(id)
-    if(!course){
-      return next(new AppError('course not found by id', 400))
-    }
-
-    res.status(200).json({
-      success: true,
-      message: 'course deleted successfully'
-    })
-
-  }catch(e){
-    return next (new AppError('e.message', 400))
-  }
-
-}
-
-// add course lecture in course  
-export const addLectureToCourseById = async (req, res, next) => {
-  
-  try{
-    const{id} = req.params
-    console.log("add lecture id is: ", id)
-    const{title, decription} = req.body
-   // console.log(req.body)
-   if(!title || !decription){
-    return next(new AppError('all fields mandatory', 400))
-   }
-
-   const course = await Course.findById(id)
-  // console.log("course is", typeof course)
-
-   if(!course){
-    return next(new AppError('course not found ', 500))
-   }
-
-   const lectureData = {
+  const updatedCourse = await Course.findByIdAndUpdate(courseId, {
     title,
-    decription,
-    lecture: {}
-    
-   }
- // console.log("data is: ", lectureData)
+		subtitle,
+		description,
+		price,
+  },{new : true})
 
-  if(req.file){
-     console.log("file is: ", req.file)  
-     
-       try{
-              const result = await cloudinary.v2.uploader.upload(req.file.path, {
-                folder: 'lms',
-                resource_type: "video"
-              })
-
-              console.log("result is: ", result)
-
-               if(result) {
-               lectureData.lecture.public_id = result.public_id,
-                lectureData.lecture.secure_url =result.secure_url
-            }
-
-            fs.rmSync(`uploads/${req.file.filename}`);
-
-            }catch(e){
-               return next(new AppError(e.message, 400))
-            }
-        }
-       
-       // console.log(lectureData)
-        course.lectures.push(lectureData)
-
-        course.numberOfLectures =  course.lectures.length;
-
-
-        await course.save();
-
-        res.status(200).json({
-          success: true,
-          message: 'lecture is added in course successfully',
-          course
-        })
-
-
-  }catch(e){
-    return next(new AppError(e.message, 400))
-  }
-    
-
+  if(!updatedCourse){
+    throw new AppError("Course not found ", 404)
   }
 
-// remove lecture from the course
-export const deleteLecture = async(req, res, next) => {
-  try{
-    const {courseId, lectureId} = req.params
-    console.log(req.params)
+  return res.status(200).json({
+    success: true,
+    message: "Course updated successfully",
+    data: updatedCourse
+  })
+
+})
+
+// only INSTRACTOR
+export const deleteCourse = asyncHandler(async(req, res) => {
+  const courseId = req.params
+
+  const course = await Course.findByIdAndDelete(courseId)
+  if(course){
+    throw new AppError("Course not found ", 404)
+  }
+  return res.status(200).json({
+    success: true,
+    message: "Course deleted Successfully",
+    data: course
+  })
   
-     const course = await Course.findById(courseId)
-       if(!course){
-        // console.log("course is : ", course)
-      return next(new AppError('course not found', 400))
-    }
-
-     course.lectures = course.lectures.filter(lecture => lecture._id.toString() !== lectureId)
-   
-     course.numberOfLectures = course.lectures.length;
-
-     await course.save();
+})
 
 
-    res.status(200).json({
-      success: true,
-      message: 'lecture deleted successfully'
-    })
 
-  }catch(e){
-    return next (new AppError(e.message, 500))
-  }
-}
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
--- server/routes/course.routes.js
+++ server/routes/course.routes.js
@@ -2,20 +2,13 @@ import express from 'express'
 
 const courseRoutes = express.Router()
 
-import {getAllCourse, getLecturesByCourseId, createCourse, updateCourse, removeCourse, addLectureToCourseById, deleteLecture} from '../controller/course.controller.js'
+import { createCourse} from '../controller/course.controller.js'
 import uplaod from '../middlewares/multer.middlewares.js'
 import {isLoggedIn, authorizedRoles} from '../middlewares/auth.middleware.js'
 
 
 courseRoutes.post('/create-course', isLoggedIn, authorizedRoles('INSTRUCTOR'), uplaod.single('thumbnail'),  createCourse)
-courseRoutes.get('/', getAllCourse)
-courseRoutes.get('/:id', isLoggedIn, getLecturesByCourseId)
 
-courseRoutes.put('/update/:id', isLoggedIn, authorizedRoles('ADMIN'), uplaod.single('thumbnail'), updateCourse)
-courseRoutes.delete('/:id', isLoggedIn, authorizedRoles('ADMIN'), removeCourse)
-
-courseRoutes.post('/:id', isLoggedIn, authorizedRoles('ADMIN'), uplaod.single('lecture'),  addLectureToCourseById)
-courseRoutes.delete('/:courseId/lectures/:lectureId', isLoggedIn, authorizedRoles('ADMIN'), deleteLecture)
 
 
 export default courseRoutes
